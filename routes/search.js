cf6d831f22aedb11fcfe571194ae973371aaac86
AarnavR <Aarnav.Rawat@gmail.com>
2022-04-16 14:29:34 -0400

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,7 @@ const express = require('express');
 var router = express.Router();
 var https = require('https');
 
-router.get("https://aarnavr.github.io/HackTJ9/form_render", function (req, res) {
+router.get("https://aarnavr.github.io/HackTJ9/form_render?", function (req, res) {
     search = req.query.mySearch
     res.render('search', {'search_value' : search})
 });
